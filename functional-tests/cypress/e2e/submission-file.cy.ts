b382d85143a38fa0c814ef1f12403e6fd70966d7
Mariama Oliveira <mariama.oliveira@thoughtworks.com>
2026-07-22 18:53:27 -0300

    Refactor file upload tests; enhance validation for valid and invalid Python files and update alert messages

--- functional-tests/cypress/e2e/submission-file.cy.ts
+++ functional-tests/cypress/e2e/submission-file.cy.ts
@@ -1,7 +1,12 @@
 describe('File Upload Success Flow', () => {
-  const testData = {
+  const testDataValid = {
     studentName: 'John Doe',
     filename: 'sum_two_numbers.py',
+  };
+
+  const testDataInValid = {
+    studentName: 'John Doe',
+    filename: 'sum_two_numbers_wrong.py',
   };
 
   beforeEach(() => {
@@ -10,16 +15,32 @@ describe('File Upload Success Flow', () => {
 
   it('should display success message after uploading and submitting a valid Python file', () => {
     // Fill in student name
-    cy.get('#student-input').type(testData.studentName);
+    cy.get('#student-input').type(testDataValid.studentName);
 
     // Upload the valid Python file
-    cy.get('#file-upload').selectFile(`cypress/fixtures/${testData.filename}`);
+    cy.get('#file-upload').selectFile(`cypress/fixtures/${testDataValid.filename}`);
 
     // Submit the form
     cy.get('[name="submit"]').click();
 
     // Verify success message appears
     cy.get('[role="alert"]').should('be.visible');
-    cy.get('[role="alert"]').should('contain', 'File received');
+    cy.get('[role="alert"]').should('contain', 'File executed successfully and passed all tests.');
+  });
+
+
+  it('should display error message after uploading and submitting an invalid Python file', () => {
+    // Fill in student name
+    cy.get('#student-input').type(testDataInValid.studentName);
+
+    // Upload the invalid Python file
+    cy.get('#file-upload').selectFile(`cypress/fixtures/${testDataInValid.filename}`);
+
+    // Submit the form
+    cy.get('[name="submit"]').click();
+
+    // Verify error message appears
+    cy.get('[role="alert"]').should('be.visible');
+    cy.get('[role="alert"]').should('contain', 'Submission Failed');
   });
 });
