a8cff9219393f199b34d67770204c1b0ab277adb
Mariama Oliveira <mariama.oliveira@thoughtworks.com>
2026-07-23 15:27:13 -0300

    Update timestamps in tests to include microseconds and adjust date format

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -13,14 +13,14 @@ const fakeSubmissions = [
     file_name: 'solution.py',
     status: 'SUCCESS',
     result_execution: 'Executed successfully',
-    created_at: '2024-06-01 10:00:00',
+    created_at: '2024-06-01 10:00:00.000000',
   },
   {
     student_name: 'John Doe',
     file_name: 'solution_2.py',
     status: 'FAILED',
     result_execution: 'An error occurred during execution',
-    created_at: '2024-06-02 11:00:00',
+    created_at: '2024-06-02 11:00:00.000000',
   },
 ]
 
@@ -41,15 +41,15 @@ describe('App', () => {
     mockedGetSubmissions.mockResolvedValueOnce(fakeSubmissions)
     render(<App />)
 
-    expect(await screen.findByRole('row', { name: /john doe solution.py success executed successfully 2024-06-01 10:00:00/i })).toBeInTheDocument()
-    expect(await screen.findByRole('row', { name: /john doe solution_2.py failed an error occurred during execution 2024-06-02 11:00:00/i })).toBeInTheDocument()
+    expect(await screen.findByRole('row', { name: /john doe solution.py success executed successfully 01-06-2024 10:00:00/i })).toBeInTheDocument()
+    expect(await screen.findByRole('row', { name: /john doe solution_2.py failed an error occurred during execution 02-06-2024 11:00:00/i })).toBeInTheDocument()
 
     const newSubmission = {
       student_name: 'Jane Smith',
       file_name: 'solution_3.py',
       status: 'SUCCESS',
       result_execution: 'Executed successfully',
-      created_at: '2024-06-03 12:00:00',
+      created_at: '2024-06-03 12:00:00.000000',
     }
 
 
@@ -73,7 +73,7 @@ describe('App', () => {
       file: expect.any(File),
     })
 
-    expect(await screen.findByRole('row', { name: /jane smith solution_3.py success executed successfully 2024-06-03 12:00:00/i })).toBeInTheDocument()
+    expect(await screen.findByRole('row', { name: /jane smith solution_3.py success executed successfully 03-06-2024 12:00:00/i })).toBeInTheDocument()
   })
 
     it('should reload the table with new data when a unsuccessful new submission is made', async () => {
@@ -84,15 +84,15 @@ describe('App', () => {
     mockedGetSubmissions.mockResolvedValueOnce(fakeSubmissions)
     render(<App />)
 
-    expect(await screen.findByRole('row', { name: /john doe solution.py success executed successfully 2024-06-01 10:00:00/i })).toBeInTheDocument()
-    expect(await screen.findByRole('row', { name: /john doe solution_2.py failed an error occurred during execution 2024-06-02 11:00:00/i })).toBeInTheDocument()
+    expect(await screen.findByRole('row', { name: /john doe solution.py success executed successfully 01-06-2024 10:00:00/i })).toBeInTheDocument()
+    expect(await screen.findByRole('row', { name: /john doe solution_2.py failed an error occurred during execution 02-06-2024 11:00:00/i })).toBeInTheDocument()
 
     const newSubmission = {
       student_name: 'Jane Smith',
       file_name: 'solution_3.py',
       status: 'FAILED',
       result_execution: 'Execution failed with error: SyntaxError: invalid syntax',
-      created_at: '2024-06-03 12:00:00',
+      created_at: '2024-06-03 12:00:00.000000',
     }
 
     mockedSubmitValidation.mockRejectedValueOnce(new Error('Execution failed with error: SyntaxError: invalid syntax'))
@@ -112,6 +112,6 @@ describe('App', () => {
       file: expect.any(File),
     })
 
-    expect(await screen.findByRole('row', { name: /jane smith solution_3.py failed execution failed with error: SyntaxError: invalid syntax 2024-06-03 12:00:00/i })).toBeInTheDocument()
+    expect(await screen.findByRole('row', { name: /jane smith solution_3.py failed execution failed with error: SyntaxError: invalid syntax 03-06-2024 12:00:00/i })).toBeInTheDocument()
   })
 })
--- frontend/src/components/submission-table/SubmissionTable.test.tsx
+++ frontend/src/components/submission-table/SubmissionTable.test.tsx
@@ -8,14 +8,14 @@ const fakeSubmissions = [
     file_name: 'solution.py',
     status: 'SUCCESS',
     result_execution: 'Executed successfully',
-    created_at: '2024-06-01 10:00:00',
+    created_at: '2026-07-23 17:21:09.976010',
   },
   {
     student_name: 'John Doe',
     file_name: 'solution_2.py',
     status: 'FAILED',
     result_execution: 'An error occurred during execution',
-    created_at: '2024-06-02 11:00:00',
+    created_at: '2026-07-23 17:21:09.976010',
   },
 ];
 
@@ -34,8 +34,8 @@ describe('SubmissionTable', () => {
             expect(screen.getByRole('columnheader', { name: /result execution/i })).toBeInTheDocument()
             expect(screen.getByRole('columnheader', { name: /created at/i })).toBeInTheDocument()
 
-            expect(screen.getByRole('row', { name: /john doe solution.py success executed successfully 2024-06-01 10:00:00/i })).toBeInTheDocument()
-            expect(screen.getByRole('row', { name: /john doe solution_2.py failed an error occurred during execution 2024-06-02 11:00:00/i })).toBeInTheDocument()
+            expect(screen.getByRole('row', { name: /john doe solution.py success executed successfully 23-07-2026 17:21:09/i })).toBeInTheDocument()
+            expect(screen.getByRole('row', { name: /john doe solution_2.py failed an error occurred during execution 23-07-2026 17:21:09/i })).toBeInTheDocument()
         });
 
         it('should render text in green if status is SUCCESS and in red if status is FAILURE', ()=>{
